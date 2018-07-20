4f08e572be70290c169c0bb87f8de4d001ea11ff
kek <oleh.petryk@gmail.com>
2018-07-21 02:11:18 +0300

    scroll made

--- js/index.js
+++ js/index.js
@@ -5,22 +5,54 @@ $(document).ready(() => {
 
     window.sr = ScrollReveal({
         duration: 500,
-        origin: 'bottom',
-        distance: '10vh',
+        distance: '5vh',
         easing: 'ease-out',
         scale: 1
     });
-    sr.reveal('#iphone', {delay: 200});
+
+
+    sr.reveal('#iphone', {delay: 200,origin:'bottom'});
+
+
+    sr.reveal('#manage .text', {delay: 400,origin:'left'});
+    sr.reveal('#manage figure', {delay: 200,origin:'right'});
+
+
+    sr.reveal('#create figure',{delay:200,origin:'top'});
+    sr.reveal('#create h2, #create p',{delay:400,origin:'bottom'});
+
+
+    sr.reveal('#growth figure',{delay:200,origin:'left'});
+    sr.reveal('#growth .text',{delay:400,origin:'right'});
+
+
+
+    sr.reveal('#about h2',{delay:200,origin:'bottom'});
+    sr.reveal('#about p',{delay:400,origin:'bottom'});
+    sr.reveal('#about img',{delay:600,origin:'bottom'});
+
+
+
+    sr.reveal('#reviews .review:nth-child(1)',{delay:200,origin:'right'});
+    sr.reveal('#reviews .review:nth-child(2)',{delay:400,origin:'left'});
+    sr.reveal('#reviews .review:nth-child(3)',{delay:600,origin:'right'});
+
+
+    sr.reveal('#pricing > *',{delay:200,origin:'top'},50);
+
+
+
+
+
 
     $(function () {
         $.scrollify(
             {
                 section: 'body > section',
-                updateHash: true,
-                sectionName: "section-name",
+                updateHash: false,
                 scrollSpeed: 1100,
                 easing: "easeOutExpo",
-                scrollbars: false,
+                scrollBars: true,
                 before: function () {
                     arr[index].style.transform = 'scale(1)';
                     index = $.scrollify.currentIndex();
